c7eb89685cfd59b044caffc2f832f118414fe812
Miki-714 <mikilegendpro@gmail.com>
2025-10-06 17:42:16 +0300

    signup section code refactoring

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -26,13 +26,14 @@ export const signup = async (req, res) => {
     const hosthedPassword = await bcrypt.hash(password, salt);
     const newUser = new User({ fullName, email, password: hosthedPassword });
     if (newUser) {
-      generateToken(newUser._id, res);
-      await newUser.save();
+      const savedUser = await newUser.save();
+      generateToken(savedUser._id, res);
+
       res.status(201).json({
-        _id: newUser._id,
-        fullName: newUser.fullName,
-        email: newUser.email,
-        profilePic: newUser.profilePic,
+        _id: savedUser._id,
+        fullName: savedUser.fullName,
+        email: savedUser.email,
+        profilePic: savedUser.profilePic,
       });
     } else {
       return res.status(400).json({ message: "Invalid user data" });
