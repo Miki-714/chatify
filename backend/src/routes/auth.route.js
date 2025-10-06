dc767c93f9a42c78a736577e3afc6bf173509370
Miki-714 <mikilegendpro@gmail.com>
2025-10-06 17:16:29 +0300

    signup controller added

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,10 +1,9 @@
 import express from "express";
+import { signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.get("/Signup", (req, res) => {
-  res.send("Signup endpoint is working");
-});
+router.post("/Signup", signup);
 
 router.get("/login", (req, res) => {
   res.send("Login endpoint is working");
