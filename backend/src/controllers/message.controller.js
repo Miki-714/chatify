18363c7eb7e93e34dbd423282a45e444f0cdd4ad
Miki-714 <mikilegendpro@gmail.com>
2025-10-09 17:59:28 +0300

    message routes optimaized

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import Message from "../models/Message.js";
 import User from "../models/User.js";
 import cloudinary from "../lib/cloudinary.js";
@@ -37,6 +38,25 @@ export const sendMessage = async (req, res) => {
     const { text, image } = req.body;
     const { id: receiverId } = req.params;
     const senderId = req.user._id;
+
+    if (!mongoose.Types.ObjectId.isValid(receiverId)) {
+      return res.status(400).json({ message: "Invalid recipient id" });
+    }
+
+    if (!text && !image) {
+      return res
+        .status(400)
+        .json({ message: "Message text or image is required" });
+    }
+    if (senderId.equals(receiverId)) {
+      return res
+        .status(400)
+        .json({ message: "Cannot send message to yourself" });
+    }
+    const receiverExists = await User.exists({ _id: receiverId });
+    if (!receiverExists) {
+      return res.status(404).json({ message: "Receiver not found" });
+    }
 
     let imageUrl;
     if (image) {
