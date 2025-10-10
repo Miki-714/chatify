2f1647de0289cdcc1e2cc9935a8229629d8cfcc5
Miki-714 <mikilegendpro@gmail.com>
2025-10-10 17:26:55 +0300

    signup implemented on the frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,12 +5,14 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
+import cors from "cors";
 
 const app = express();
 const __dirname = Path.resolve();
 
 const PORT = ENV.PORT || 3000;
 app.use(express.json());
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
