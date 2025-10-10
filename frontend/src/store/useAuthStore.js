6aa36674b2bb01eb9757b9cc29c434f23bd5877c
Miki-714 <mikilegendpro@gmail.com>
2025-10-10 19:20:55 +0300

    make consistency

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -20,29 +20,29 @@ export const useAuthStore = create((set) => ({
     }
   },
 
-  signup: async (Data) => {
+  signup: async (data) => {
     set({ isSigningUp: true });
     try {
-      const res = await axiosInstance.post("/auth/signup", Data);
+      const res = await axiosInstance.post("/auth/signup", data);
       set({ authUser: res.data });
 
       toast.success("Account created successfully!");
     } catch (error) {
-      toast.error(error.response.data.message);
+      toast.error(error.response?.data?.message || "signing up failed");
     } finally {
       set({ isSigningUp: false });
     }
   },
 
-  login: async (Data) => {
+  login: async (data) => {
     set({ isLoggingIn: true });
     try {
-      const res = await axiosInstance.post("/auth/login", Data);
+      const res = await axiosInstance.post("/auth/login", data);
       set({ authUser: res.data });
 
       toast.success("Logged in successfully");
     } catch (error) {
-      toast.error(error.response.data.message);
+      toast.error(error.response?.data?.message || "Login failed");
     } finally {
       set({ isLoggingIn: false });
     }
